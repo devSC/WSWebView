6acf198e260829f84f00990e24d3f8c1310ab74f
YSC <yuanshichong@wowsai.com>
2015-09-06 18:02:16 +0800

    Update

--- WSWebView-Demo/WSWebView-Demo/WSWebView.h
+++ WSWebView-Demo/WSWebView-Demo/WSWebView.h
@@ -8,9 +8,25 @@
 
 #import <UIKit/UIKit.h>
 
+
+@protocol WSWebViewDelegate;
+
 @interface WSWebView : UIView
+
+@property (nonatomic, assign) id<WSWebViewDelegate> delegate;
 
 - (void)ws_loadRequest: (NSURLRequest *)request;
 
+@end
+
+@protocol WSWebViewDelegate <NSObject>
+
+- (void)wswebView: (WSWebView *)webView didStartLoadWithUrl: (NSURL *)url;
+
+- (void)wswebView: (WSWebView *)webView didFinisheLoadWithUrl: (NSURL *)url;
+
+- (void)wswebView: (WSWebView *)webView didFailLoadWithError: (NSError *)error;
+
+- (void)wswebView: (WSWebView *)webView shouldStartLoadWithRequest: (NSURLRequest *)request navigationType: (UIWebViewNavigationType)type;
 
 @end
